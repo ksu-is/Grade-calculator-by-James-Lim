a6ef2774ae3a5f73d082c96f52c2a9a30d0750b6
Jameslim845 <Jlim24@students.kennesaw.edu>
2024-11-14 18:33:31 -0500

    Update Coding project for app.py

--- Coding project for app.py
+++ Coding project for app.py
@@ -1,22 +1,23 @@
 #This will serve as my code in the visual studio code
-import statistics
+import statistics #Built in function to calculate mathematical statistics of numeric data.
 grades = []
-classes = input("What class are you trying to calculate your grade in? ")
+while():
+    classes = input("What class are you trying to calculate your grade in? ")
+
 
 while len(grades) < 5: # counts up the total attempts grades has untill its over 5
-    user_input = input(f"Enter the score for the last 5 assignment.  ")
-    user_input = len(grades) + 1
+    user_input = input(f"Enter the score for assignment {len(grades) + 1}: ") # F string used to help append items until into the list
     user_input = int(user_input)
     if 0 <= user_input <= 100:  # Make sure the score is between 0 and 100
         grades.append(user_input)
     else:
         print("Please enter a number between the ranges of 0-100")
-
+print(grades)
 grades_score = statistics.mean(grades)
 print(grades_score)
-def system(grades_score): 
+def system(grades_score): # This is where the grade is scored on a letter basis
     if grades_score >= 90:
-        print("Your score for the class is currently a A") 
+        print("Your score for the class is currently an A") 
         return "A"
     elif grades_score >= 80:
         print("Your score for the class is currently a B")
