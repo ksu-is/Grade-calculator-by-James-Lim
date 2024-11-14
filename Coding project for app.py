03b04c69f7539e3af4cfd030846820d73d6cb0ca
Jameslim845 <Jlim24@students.kennesaw.edu>
2024-11-14 16:43:15 -0500

    Create Coding project for app.py

---
+++ Coding project for app.py
@@ -0,0 +1,3 @@
+#This will serve as my code in the visual studio code
+classes = input("What class are you trying to calculate your grade in? ")
+user = input("Put down the last 5 assignemts that you scored. ")
